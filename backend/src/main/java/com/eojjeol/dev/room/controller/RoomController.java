540f33374e093cb7e1306a26608ad9e09fb33331
이상윤 <zlflshdh84@gmail.com>
2022-01-31 14:06:42 +0900

    Feat: Add Room MVC Structure

---
+++ backend/src/main/java/com/eojjeol/dev/room/controller/RoomController.java
@@ -0,0 +1,42 @@
+package com.eojjeol.dev.room.controller;
+
+import com.eojjeol.dev.room.dto.RoomDto;
+import com.eojjeol.dev.room.service.RoomService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/room")
+@RequiredArgsConstructor
+public class RoomController {
+
+    private final RoomService roomService;
+
+    @PostMapping("/")
+    @PreAuthorize("hasAnyRole('USER','ADMIN')")
+    public ResponseEntity<RoomDto> createRoom(@RequestBody RoomDto roomDto) {
+        return roomService.createRoom(roomDto);
+    }
+
+    @DeleteMapping("/{id}")
+    @PreAuthorize("hasAnyRole('USER','ADMIN')")
+    public ResponseEntity<RoomDto> deleteRoom(@PathVariable Long id) {
+        return roomService.deleteRoom(id);
+    }
+
+    @GetMapping("/{id}")
+    @PreAuthorize("hasAnyRole('USER','ADMIN')")
+    public ResponseEntity<RoomDto> selectOne(@PathVariable Long id) {
+        return roomService.selectRoom(id);
+    }
+
+    @GetMapping("/")
+    @PreAuthorize("hasAnyRole('USER','ADMIN')")
+    public ResponseEntity<List<RoomDto>> selectList() {
+        return roomService.selectAll();
+    }
+}
---
+++ backend/src/main/java/com/eojjeol/dev/room/repository/RoomRepository.java
@@ -0,0 +1,18 @@
+package com.eojjeol.dev.room.repository;
+
+import com.eojjeol.dev.entity.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface RoomRepository extends JpaRepository<Room, Long> {
+    @Override
+    List<Room> findAll();
+
+    @Override
+    Optional<Room> findById(Long id);
+
+    @Override
+    void deleteById(Long id);
+}
