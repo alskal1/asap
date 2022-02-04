0c54972a122f20d5c6481698050460baa295cd0b
zlflshdh84 <zlflshdh84@gmail.com>
2022-02-04 11:02:54 +0900

    Feat: Add Point querydsl tasks

--- backend/src/main/java/com/eojjeol/dev/point/service/PointService.java
+++ backend/src/main/java/com/eojjeol/dev/point/service/PointService.java
@@ -5,6 +5,7 @@ import com.eojjeol.dev.entity.PointStatus;
 import com.eojjeol.dev.entity.member.Member;
 import com.eojjeol.dev.member.repository.MemberRepository;
 import com.eojjeol.dev.point.dto.PointDto;
+import com.eojjeol.dev.point.repository.PointQueryRepository;
 import com.eojjeol.dev.point.repository.PointRepository;
 import com.eojjeol.dev.security.util.SecurityUtil;
 import lombok.RequiredArgsConstructor;
@@ -13,51 +14,70 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-@Transactional
 public class PointService {
 
     private final MemberRepository memberRepository;
     private final PointRepository pointRepository;
+    private final PointQueryRepository pointQueryRepository;
 
+    @Transactional
     public ResponseEntity<PointDto> createPoint(PointDto pointDto) {
         try {
             Member member = SecurityUtil.getCurrentEmail().flatMap(memberRepository::findOneWithAuthoritiesByEmail).orElse(null);
+
+            if(pointDto.getStatus().equals(PointStatus.WIN)) {
+                member.setPoint(member.getPoint() - pointDto.getPrice());
+            } else {
+                member.setPoint(member.getPoint() + pointDto.getPrice());
+            }
+
             Point point = Point.builder()
                     .status(pointDto.getStatus())
                     .price(pointDto.getPrice())
                     .member(member)
                     .build();
+
             Point savePoint = pointRepository.save(point);
             PointDto savePointDto = PointDto.from(savePoint);
             return new ResponseEntity<>(savePointDto, HttpStatus.OK);
-        }catch(Exception e){
+        } catch(Exception e){
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
     }
 
-    // id =  point id
-    // we want member id
-    public ResponseEntity<PointDto> selectPoint(){
+    public ResponseEntity<List<PointDto>> selectPoint(){
         try {
-//            PointDto findPoint = PointDto.from(pointRepository.findAll());
-//            return new ResponseEntity<PointDto>(findPoint, HttpStatus.OK);
-            return null;
-        }catch(Exception e){
-            return new ResponseEntity<PointDto>(HttpStatus.BAD_REQUEST);
+            Member member = SecurityUtil.getCurrentEmail().flatMap(memberRepository::findOneWithAuthoritiesByEmail).orElse(null);
+            List<Point> pointList = pointQueryRepository.findAllPoints(member.getId());
+            List<PointDto> pointDtoList = new ArrayList<>();
+
+            for(Point point : pointList) {
+                pointDtoList.add(PointDto.from(point));
+            }
+
+            return new ResponseEntity<>(pointDtoList, HttpStatus.OK);
+        } catch(Exception e){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
     }
 
-    // where status eq status => queryDsl
-    public ResponseEntity<PointDto> selectPointByStatus(PointStatus status) {
+    public ResponseEntity<List<PointDto>> selectPointByStatus(PointStatus status) {
         try {
-//            Point point = pointRepository.findByStatus(status).orElse(null);
-//            PointDto pointDto = PointDto.from(point);
-//            return new ResponseEntity<>(pointDto, HttpStatus.OK);
-            return null;
-        }catch(Exception e){
+            Member member = SecurityUtil.getCurrentEmail().flatMap(memberRepository::findOneWithAuthoritiesByEmail).orElse(null);
+            List<Point> pointList = pointQueryRepository.findAllPointsByStatus(member.getId(), status);
+            List<PointDto> pointDtoList = new ArrayList<>();
+
+            for(Point point : pointList) {
+                pointDtoList.add(PointDto.from(point));
+            }
+
+            return new ResponseEntity<>(pointDtoList, HttpStatus.OK);
+        } catch(Exception e){
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
     }
