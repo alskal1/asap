5198431f38e4cac95df8c229a70143f7df490a0b
윤숙 <dbstnr2146@naver.com>
2022-02-05 23:20:53 +0900

    Feat: Navbar

--- frontend/src/router/routes.js
+++ frontend/src/router/routes.js
@@ -2,7 +2,28 @@ const routes = [
   {
     path: "/",
     component: () => import("layouts/MainLayout.vue"),
-    children: [{ path: "", component: () => import("pages/Index.vue") }],
+    children: [
+      {
+        path: "",
+        component: () => import("pages/Index.vue"),
+      },
+      {
+        path: "/notice",
+        component: () => import("pages/Notice.vue"),
+      },
+      {
+        path: "/auction",
+        component: () => import("pages/Auction.vue"),
+      },
+      {
+        path: "/point",
+        component: () => import("pages/ChargePoint.vue"),
+      },
+      {
+        path: "/member",
+        component: () => import("pages/Mypage.vue"),
+      },
+    ],
   },
 
   // Always leave this as last one,
