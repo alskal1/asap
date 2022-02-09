1222df774ba82b926ef11ac652b44f690853fedb
zlflshdh84 <zlflshdh84@gmail.com>
2022-02-09 21:05:29 +0900

    Feat: Add SellList

--- frontend/src/components/mypage/child/MySellList.vue
+++ frontend/src/components/mypage/child/MySellList.vue
@@ -1,33 +1,34 @@
 <template>
   <q-page>
     <div>
-      <q-table>
-        <q-tr>
-          <q-th>경매일자</q-th>
-          <q-th>품목명</q-th>
-          <q-th>판매자명</q-th>
-          <q-th>수량</q-th>
-          <q-th>금액</q-th>
-          <q-th>상태</q-th>
-        </q-tr>
-        <q-tr
+      <table>
+        <tr>
+          <th>경매일자</th>
+          <th>품목명</th>
+          <th>수량</th>
+          <th>금액</th>
+          <th>상태</th>
+        </tr>
+        <tr
           v-for="(sellItem, index) in sellHistoryList"
           :key="index"
           v-bind="sellItem"
         >
-          <q-td>{{ sellItem.createdDate }}</q-td>
-          <q-td>{{ sellItem.productName }}</q-td>
-          <q-td>{{ sellItem.sellerId }}</q-td>
-          <q-td>{{ sellItem.count }}</q-td>
-          <q-td>{{ sellItem.finalPrice }}</q-td>
-          <q-td>{{ sellItem.deliveryState }}</q-td>
-        </q-tr>
-      </q-table>
+          <td>{{ sellItem.date }}</td>
+          <td>{{ sellItem.productName }}</td>
+          <td>{{ sellItem.count }}</td>
+          <td>{{ sellItem.finalPrice }}</td>
+          <td>{{ sellItem.deliveryState }}</td>
+        </tr>
+      </table>
     </div>
   </q-page>
 </template>
 
 <script>
+import { computed, onMounted } from "@vue/runtime-core";
+import { useStore } from "vuex";
+
 export default {
   name: "MySellList",
   setup() {
