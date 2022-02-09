a9499d17e56d823330a8048612c4af25f3b45416
uiyn <uiyn@naver.com>
2022-02-09 12:11:19 +0900

    Feat: Add HistoryDesign

--- frontend/src/components/mypage/MySellHistory.vue
+++ frontend/src/components/mypage/MySellHistory.vue
@@ -1,5 +1,20 @@
 <template>
-  <q-page>판매내역</q-page>
+  <q-page>
+    <q-card>
+      <div class="row q-pa-md">
+        <div class="q-pa-md q-mr-lg">
+          <q-avatar rounded size="200px" color="teal" icon="none" />
+        </div>
+        <div class="text-h6 q-pa-md">
+          <p>무안 황토 고구마 10KG (낙찰 시간 : 2022년 02월 08일 08:13:07)</p>
+          <p>낙찰 수량 : 3</p>
+          <p>낙찰 포인트 : 8200 포인트</p>
+
+          <p>구매자 정보 : 김아삽</p>
+        </div>
+      </div>
+    </q-card>
+  </q-page>
 </template>
 
 <script>
---
+++ frontend/src/components/mypage/child/MyWinList.vue
@@ -0,0 +1,58 @@
+<template>
+  <q-table class="q-mt-md" :rows="rows" :columns="columns" row-key="name" />
+</template>
+
+<script>
+const columns = [
+  {
+    name: "auctionDate",
+    align: "center",
+    label: "경매일자",
+    field: "auctionDate",
+  },
+  { name: "productName", align: "center", label: "품목명", field: "name" },
+  { name: "seller", align: "center", label: "판매자명", field: "seller" },
+  { name: "quantity", align: "center", label: "수량", field: "sodium" },
+  { name: "state", align: "center", label: "상태", field: "sodium" },
+];
+
+const rows = [
+  {
+    name: "Frozen Yogurt",
+    auctionDate: 20220208,
+    name: 7.0,
+    carbs: 24,
+    protein: 4.0,
+  },
+  {
+    name: "Ice cream sandwich",
+    auctionDate: 20220208,
+    name: 9.0,
+    carbs: 37,
+    protein: 4.3,
+  },
+  {
+    name: "Eclair",
+    auctionDate: 20220208,
+    name: 16.0,
+    carbs: 23,
+    protein: 6.0,
+  },
+  {
+    name: "Cupcake",
+    auctionDate: 20220208,
+    name: 3.7,
+    carbs: 67,
+    protein: 4.3,
+  },
+];
+export default {
+  name: "MyWinList",
+  setup() {
+    return {
+      columns,
+      rows,
+    };
+  },
+};
+</script>
