ad77e2fbf3a9a584ea72a131ac05f5d7cfee8499
uiyn <uiyn@naver.com>
2022-02-10 17:08:03 +0900

    Design: Update sellList design

---
+++ frontend/src/components/mypage/child/SellListItem.vue
@@ -0,0 +1,25 @@
+<template>
+  <q-item>
+    <q-item-section class="col-2">{{ sellItem.date }}</q-item-section>
+    <q-item-section class="col-3">{{ sellItem.productName }}</q-item-section>
+    <q-item-section class="col-2">{{ sellItem.sellerId }}</q-item-section>
+    <q-item-section class="col-1">{{ sellItem.count }}</q-item-section>
+    <q-item-section class="col-2">{{ sellItem.finalPrice }}</q-item-section>
+    <q-item-section class="col-1">{{ sellItem.deliveryState }}</q-item-section>
+  </q-item>
+  <q-separator />
+</template>
+
+<script>
+export default {
+  name: "SellListItem",
+  props: {
+    sellItem: {
+      type: Object,
+    },
+  },
+  setup(props) {
+    return {};
+  },
+};
+</script>
