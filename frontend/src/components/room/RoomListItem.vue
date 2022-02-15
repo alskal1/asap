9ffcfb3b0bb7b08e4aa70f58359987dc64feddc8
zlflshdh84 <zlflshdh84@gmail.com>
2022-02-15 22:44:57 +0900

    Feat: Change base img and Add hover effect

--- frontend/src/components/room/RoomListItem.vue
+++ frontend/src/components/room/RoomListItem.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="q-pa-md">
-    <q-card style="max-width: 400px">
-      <img src="https://cdn.quasar.dev/img/mountains.jpg" />
+    <q-card class="scale" style="max-width: 400px">
+      <img
+        src="https://img.freepik.com/free-vector/antique-auction-isometric-composition_1284-22062.jpg"
+      />
       <q-card-section>
         <div class="text-h6">{{ room.title }}</div>
         <div class="text-subtitle2">{{ room.description }}</div>
@@ -23,3 +25,22 @@ export default {
   },
 };
 </script>
+<style>
+.scale {
+  transform: scale(1);
+  -webkit-transform: scale(1);
+  -moz-transform: scale(1);
+  -ms-transform: scale(1);
+  -o-transform: scale(1);
+  transition: all 0.3s ease-in-out; /* 부드러운 모션을 위해 추가*/
+}
+
+.scale:hover {
+  cursor: pointer;
+  transform: scale(1.1);
+  -webkit-transform: scale(1.1);
+  -moz-transform: scale(1.1);
+  -ms-transform: scale(1.1);
+  -o-transform: scale(1.1);
+}
+</style>
