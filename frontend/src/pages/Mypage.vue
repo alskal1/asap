7184d59d3e745e36908581dd8002f94facf0e677
윤숙 <dbstnr2146@naver.com>
2022-02-16 10:23:44 +0900

    Fix : design mypage

--- frontend/src/pages/Mypage.vue
+++ frontend/src/pages/Mypage.vue
@@ -1,54 +1,59 @@
 <template>
   <q-page padding>
-    <div class="row flex justify-center">
-      <div class="col-12 q-pa-xs" style="height: 200px; text-align: center">
-        <q-avatar
-          icon-right="colorize"
-          size="250px"
-          text-color="green-4"
-          icon="account_circle"
+    <q-card>
+      <div class="row flex justify-center">
+        <div class="col-12 q-pa-xs" style="height: 200px; text-align: center">
+          <q-avatar
+            icon-right="colorize"
+            size="250px"
+            text-color="green-4"
+            icon="account_circle"
+          >
+          </q-avatar>
+        </div>
+        <div style="text-align: center" class="q-md-lg col-12 text-h5">
+          {{ userInfo.name }}
+        </div>
+        <div
+          style="text-align: center"
+          class="text-grey q-md-lg col-12 text-h7"
         >
-        </q-avatar>
+          {{ userInfo.email }}
+        </div>
       </div>
-      <div style="text-align: center" class="q-md-lg col-12 text-h5">
-        {{ userInfo.name }}
+
+      <div class="q-pt-lg">
+        <q-tabs
+          v-model="mypageTab"
+          dense
+          class="text-green"
+          align="justify"
+          narrow-indicator
+        >
+          <q-tab name="mySell" label="판매내역" />
+          <q-tab name="myWin" label="낙찰내역" />
+          <q-tab name="myPoint" label="내 포인트" />
+          <q-tab name="myInfo" label="내 정보" />
+        </q-tabs>
       </div>
-      <div style="text-align: center" class="text-grey q-md-lg col-12 text-h7">
-        {{ userInfo.email }}
-      </div>
-    </div>
 
-    <div class="q-pt-lg">
-      <q-tabs
-        v-model="mypageTab"
-        dense
-        class="text-green"
-        align="justify"
-        narrow-indicator
-      >
-        <q-tab name="mySell" label="판매내역" />
-        <q-tab name="myWin" label="낙찰내역" />
-        <q-tab name="myPoint" label="내 포인트" />
-        <q-tab name="myInfo" label="내 정보" />
-      </q-tabs>
-    </div>
+      <q-separator />
 
-    <q-separator />
-
-    <q-tab-panels v-model="mypageTab" animated>
-      <q-tab-panel name="mySell">
-        <my-sell-history />
-      </q-tab-panel>
-      <q-tab-panel name="myWin">
-        <my-win-history />
-      </q-tab-panel>
-      <q-tab-panel name="myPoint">
-        <my-point-history />
-      </q-tab-panel>
-      <q-tab-panel name="myInfo">
-        <my-info-update />
-      </q-tab-panel>
-    </q-tab-panels>
+      <q-tab-panels v-model="mypageTab" animated>
+        <q-tab-panel name="mySell">
+          <my-sell-history />
+        </q-tab-panel>
+        <q-tab-panel name="myWin">
+          <my-win-history />
+        </q-tab-panel>
+        <q-tab-panel name="myPoint">
+          <my-point-history />
+        </q-tab-panel>
+        <q-tab-panel name="myInfo">
+          <my-info-update />
+        </q-tab-panel>
+      </q-tab-panels>
+    </q-card>
   </q-page>
 </template>
 
