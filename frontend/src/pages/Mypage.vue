194db1538c1eec4ae1f061056d58974210472d92
윤숙 <dbstnr2146@naver.com>
2022-02-16 09:40:47 +0900

    Feat : Add tracking delivery

--- frontend/src/pages/Mypage.vue
+++ frontend/src/pages/Mypage.vue
@@ -1,15 +1,38 @@
 <template>
-  <q-page>
-    <q-toolbar class="bg-green-3 text-white">
-      <q-btn size="20px" flat>{{ userInfo.name }} 의 페이지</q-btn>
-      <q-space />
-      <q-tabs v-model="mypageTab">
+  <q-page padding>
+    <div class="row flex justify-center">
+      <div class="col-12 q-pa-xs" style="height: 200px; text-align: center">
+        <q-avatar
+          icon-right="colorize"
+          size="250px"
+          text-color="green-4"
+          icon="account_circle"
+        >
+        </q-avatar>
+      </div>
+      <div style="text-align: center" class="q-md-lg col-12 text-h5">
+        {{ userInfo.name }}
+      </div>
+      <div style="text-align: center" class="text-grey q-md-lg col-12 text-h7">
+        {{ userInfo.email }}
+      </div>
+    </div>
+
+    <div class="q-pt-lg">
+      <q-tabs
+        v-model="mypageTab"
+        dense
+        class="text-green"
+        align="justify"
+        narrow-indicator
+      >
         <q-tab name="mySell" label="판매내역" />
         <q-tab name="myWin" label="낙찰내역" />
         <q-tab name="myPoint" label="내 포인트" />
         <q-tab name="myInfo" label="내 정보" />
       </q-tabs>
-    </q-toolbar>
+    </div>
+
     <q-separator />
 
     <q-tab-panels v-model="mypageTab" animated>
