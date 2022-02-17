ca36cedd0d7e4fbca5bc736b8d1c92cd56d1b49c
윤숙 <dbstnr2146@naver.com>
2022-02-17 23:00:14 +0900

    Design: fix index-page

--- frontend/src/pages/Index.vue
+++ frontend/src/pages/Index.vue
@@ -2,7 +2,7 @@
   <q-page padding>
     <banner></banner>
     <div class="row">
-      <div class="text-h5 text-bold q-px-md q-pt-sm">현재 진행중인 경매</div>
+      <div class="text-h5 text-bold q-px-md q-pt-md">실시간 경매</div>
     </div>
     <q-separator class="q-my-md" />
     <room-list></room-list>
